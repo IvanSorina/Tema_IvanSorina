af18a80dbcc7e362e2e70bc30055636f10840f80
IvanSorina <134533131+IvanSorina@users.noreply.github.com>
2023-05-25 14:57:46 -0700

    Tema ppt

---
+++ src/com/company/Main.java
@@ -0,0 +1,173 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //Tema Site
+//System.out.println("Hello");
+//System.out.println("Alexandra Abramov"); //ex 1
+// int a=74;
+//int b=36;
+//System.out.println(a+b);   //ex 2
+//int a1=50;
+//int a2=3;
+//System.out.println(a1/a2); //ex 3
+//int a3=-5;
+//int a4=8;
+//int a5=6;
+//System.out.println(a3+a4*a5); //ex 4 a)
+//int a6=55;
+//int a7=9;
+//System.out.println((a6+a7)%a7);//ex 4 b)
+//int a8=20;
+//int a9=-3;
+//int a10=5;
+//int a11=8;
+//System.out.println(a8+a9*a10/a11); // ex 4 c)
+//int a12=5;
+//int a13=15;
+//int a14=3;
+//int a15=2;
+//int a16=-8;
+//int a17=3;
+//System.out.println(a12+a13/a14*a15+a16%a17); //ex 4 d)
+// System.out.println("25 x 5 = 125"); //ex 5
+//         System.out.println("125 + 24 = 149");
+//        System.out.println("125 - 24 = 101");
+//        System.out.println ("125 x 24 = 3000");
+
+        //Tema ppt
+        //        int[] tablou = {1, 2, 3, 8, 7, 6};
+//        tablou[3] = 4;
+//        tablou[4] = 5;
+//
+//
+//        for (int i = 0; i < tablou.length; i = i + 1) {
+//            System.out.print(tablou[i] + "");
+//        }
+
+//        integer[ ] array = {2,4,6,8};
+//        boolean b = Arrays.asList(array).contains(4);
+//        System.out.println(b);
+//
+//
+////        String[ ] array = {"a", "b", "c", "d"};
+////        boolean b = Arrays.asList(array).contains("d");
+////        System.out.println(b);
+
+//        int[] sir = {1, 5, 2, 8, 9, 14, 3, 4, 17, 10, 18};
+//
+//        int maxim = 0;
+//
+//        for (int i = 0; i < sir.length; i = i + 1) {
+//            if (sir[i] > maxim) {
+//                maxim = sir[i];
+//            }
+//        }
+//        System.out.println("Maximul este" + maxim);
+
+//        int nr=5;
+//        if(nr>=5 && nr<=10){
+//            System.out.println("Numarul" + nr + "e afla in intervalul [5,10]");
+//        }
+//        else{
+//            System.out.println("Numarul" + nr + "nu se afla in intervalul [5,10]");
+//        }
+//      Numere PARE:
+//        int[] sir = {0, 1, 2, 3, 4, 5, 6, 7};
+//
+//        for (int i = 0; i < sir.length; i = i + 2) {
+//
+//            System.out.print(sir[i] + "");
+//        }
+////      Numere IMPARE
+//        int[] sir1 = {0, 1, 2, 3, 4, 5, 6, 7};
+//
+//        System.out.println();
+//
+//        for (int i = 1; i < sir1.length; i = i + 2) {
+//
+//            System.out.print(sir1[i] + "");
+
+//        Incremantare Numere Impare
+//
+//        int[] sir2 = {0, 1, 2, 3, 4, 5, 6, 7};
+//
+//        for (int i=1; i < sir2.length; i = i + 2) {
+//            sir2[i] = sir2[i] + 1;
+//
+//            System.out.println(sir2[i] + "");
+//        }
+//
+//
+//        System.out.println();
+//        //Decrementare Numere Pare
+//
+//        int[] sir3 = {0, 1, 2, 3, 4, 5, 6, 7};
+//
+//        for (int i=0; i < sir2.length; i = i + 2) {
+//            sir2[i] = sir2[i] - 1;
+//
+//            System.out.println(sir2[i] + "");
+//        }
+        //Slide 59
+        //Exercitiu 1
+
+//        String a = "Maria merge cu Bicicleta";
+//
+//        System.out.println(a.charAt(7));
+//
+//        //Exercitiu 2
+//
+//        String b = "Azi este o zi minunata";
+//
+//        System.out.println(b.replace("z","t"));
+//
+//        //Exercitiu 3
+//
+//        String c = "Mere";
+//
+//        System.out.println(c.replaceFirst("e","a"));
+//
+//        //Exercitiu 4
+//
+//        String d = "236524";
+//
+//        System.out.println(d.substring(2,4));
+//
+//
+//
+////       //Exercitiu 5
+//        for (int i = 100; i >= 0; i = i - 2) {
+//            System.out.print(i + "    ");
+//        }
+        //Exercitiu 6
+//        String[] sir = {"mere", "pere", "visine", "portocale"};
+//        for(int i=0;i<sir.length;i++)
+//        System.out.print(sir[i]+" ");
+
+//        //Exercitiu 7
+//        int k=0;
+//        for (int i=2;i<100;i+=2)
+//        {
+//            System.out.print(i+ ",");
+//            k=k+1;
+//        }
+//        System.out.println();
+//        System.out.println(k + " ");
+//
+//
+//
+//        Exercițiu 8
+//        int n=10;
+//        int s=((n*(n+1))/2);
+//        System.out.println("Valoarea sumei numarului n este = " +s);
+
+
+    }
+}
+
